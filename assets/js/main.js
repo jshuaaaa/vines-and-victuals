8ffd8c8584018cfdec9f76cf768c4f58345549db
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-07-07 11:18:01 -0400

    added modal and setup user input settings

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,69 @@
 const search = document.getElementById('search')
 const searchButton = document.getElementById('searchButton')
-
-const options = {
+const food = document.getElementById('food')
+const drink = document.getElementById('drink')
+const saveButton = document.getElementById('save-button')
+let url = 'https://cocktails3.p.rapidapi.com/random'
+options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'c36c798c41msh6e4944725bbf051p1c3342jsn7e587c0ecbdc',
-		'X-RapidAPI-Host': 'cocktails3.p.rapidapi.com'
+		'X-RapidAPI-Key': 'c36c798c41msh6e4944725bbf051p1c3342jsn7e587c0ecbdc'
 	}
-};
+}
 
-searchButton.addEventListener("click", getDrinks)
+
+function settingsChecker(e) {
+	e.preventDefault()
+	if(food.checked) {
+		console.log('e')
+		url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegetarian%2Cdessert&number=1'
+		options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': 'c36c798c41msh6e4944725bbf051p1c3342jsn7e587c0ecbdc',
+			}
+		}
+	}
+
+	if(drink.checked) {
+		console.log('d')
+		url = 'https://cocktails3.p.rapidapi.com/random'
+		options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': 'c36c798c41msh6e4944725bbf051p1c3342jsn7e587c0ecbdc'
+			}
+		}
+	}
+}
+
+saveButton.addEventListener("click", settingsChecker)
+
+searchButton.addEventListener("click", function(){
+	if (url === 'https://cocktails3.p.rapidapi.com/random') {
+		getDrinks()
+
+	} else
+	getFood()
+})
 
 function getDrinks() {
-fetch('https://cocktails3.p.rapidapi.com/random', options)
+	fetch(url, options)
 	.then(response => response.json())
 	.then(function(response){
         console.log(response)
-        console.log(response.body[0].name)
     })
 	.catch(err => console.error(err));
 }
+
+function getFood() {
+	console.log('getfood was called')
+	fetch(url, options)
+	.then(response => response.json())
+	.then(function(response){
+        console.log(response)
+    })
+	.catch(err => console.error(err));
+
+}
+
