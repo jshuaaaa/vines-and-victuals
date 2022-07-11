96c73dd47049e845718aea39f92cd60ab1f69b58
KRivera2394 <keanurivera2394@gmail.com>
2022-07-10 21:40:13 -0400

    SavedDATA

--- assets/js/save.js
+++ assets/js/save.js
@@ -1,10 +1,9 @@
 //
 var storedRecipe = JSON.parse(localStorage.storedRecipe)
-console.log(storedRecipe);
-console.log(localStorage.storedRecipe);
+
 
 for( var i=0 ; i < storedRecipe.length ; i++){
     $('<a>', {
-        href:"./single.html/" + storedRecipe[i].url
+        href:"./single.html" + storedRecipe[i].url
     } ).appendTo("#stored") 
 }  
