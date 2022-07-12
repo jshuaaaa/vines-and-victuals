7176b121dacf83f7a0dd50d47532dbc1686b10b4
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-07-12 19:11:12 -0400

    removed google image api as it was giving inappropriate content for certain drinks

--- assets/js/single.js
+++ assets/js/single.js
@@ -39,19 +39,19 @@ console.log(drinkName)
   
 
         //Function used to get an image for our drinks as the API doesnt have a premade image for them!
-    function getApiDrinkImage() {
-        fetch(url9, options)
-        .then(response => response.json())
-        .then(function(response){
-            console.log(response)
-            $('<img>', {
-                src: response.value[0].thumbnail
-            }).appendTo('#ingredient-list')
+    // // function getApiDrinkImage() {
+    // //     fetch(url9, options)
+    // //     .then(response => response.json())
+    // //     .then(function(response){
+    // //         console.log(response)
+    // //         $('<img>', {
+    // //             src: response.value[0].thumbnail
+    // //         }).appendTo('#ingredient-list')
         
-            image = response.value[0].thumbnail
+    // //         image = response.value[0].thumbnail
     
             
-    })}
+    // })} Used for presentation however images are displaying inappropriate content so I am commenting it out for now and adding a placeholder
     
 //Function called when user is on a drink related page
 function getApiSingleForDrink() {
@@ -70,7 +70,9 @@ function getApiSingleForDrink() {
             $('<p>').appendTo('#ingredient-list').text(response.body[0][0].ingredients[index])
         }
 
-       
+       $('<img>', {
+        src: 'https://www.ohlq.com/ui/dist/assets/toolkit/images/product/no-product-image.png'
+       }).appendTo('#ingredient-list')
         // the below code is an algorithm used to get foods with similar ingredients as the current drink
         let ingredient;
 		var i = 0
